0e834c659226715e2b962c2a7c9a1459d345d49c
sherrieguan <35797658+sherrieguan@users.noreply.github.com>
2018-02-09 21:36:51 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,36 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+}
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(251, 99, 126);
+  } else if (key == 'G') {
+    fill(167, 225, 99);
+  } else if (key == 'B') {
+    fill(72, 159, 223); 
+  } else if (key == 'A') {
+    fill(248, 190, 214); 
+  } else if (key == 'S') {
+    fill(185, 153, 214); 
+  } else if (key == 'D') {
+    fill(255, 228, 149); 
+  }
+}
