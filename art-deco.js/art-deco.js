1dee5d46f71bbafc52e9ddae046081c0cf3dccf4
sherrieguan <35797658+sherrieguan@users.noreply.github.com>
2018-02-10 17:02:37 -0800

    Create art-deco.js

---
+++ art-deco.js/art-deco.js
@@ -0,0 +1,11 @@
+for (var i = 10; i <= 320; i = i+65) {
+   for (var x = 50; x >= 0; x = x-10) {
+//  var startX = i;
+//  var startY = 170;
+//  var endX = 50;
+//  var endY = 50;
+  rect(340-x-i, 170-x, x, x);
+  
+
+  }
+}
